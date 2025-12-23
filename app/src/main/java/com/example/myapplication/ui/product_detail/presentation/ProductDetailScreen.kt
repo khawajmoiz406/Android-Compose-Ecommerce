71ad39e2a82cab752cd2651d564485056bede89c
khawajamoiz <khawaja.moiz@sixlogics.com>
2025-12-23 19:22:18 +0500

    - Added tab for images in detail screen top bar. - Changes in UI of detail screen.

--- app/src/main/java/com/example/myapplication/ui/product_detail/presentation/ProductDetailScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/product_detail/presentation/ProductDetailScreen.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication.ui.product_detail.presentation
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,35 +12,60 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
+import com.example.myapplication.R
+import com.example.myapplication.di.createApiService
+import com.example.myapplication.di.createOkHttpClient
+import com.example.myapplication.di.createRetrofit
+import com.example.myapplication.models.response.product.Dimensions
+import com.example.myapplication.models.response.product.Meta
+import com.example.myapplication.models.response.product.Product
+import com.example.myapplication.models.response.product.Review
+import com.example.myapplication.ui.product_detail.data.ProductDetailRemoteRepoImpl
+import com.example.myapplication.ui.product_detail.domain.GetProductDetailUseCase
 import com.example.myapplication.ui.product_detail.presentation.components.ProductDetailTopBar
 import com.example.myapplication.utils.AppCompositionLocals.LocalParentNavController
 import com.example.myapplication.utils.ComposableUtils.topShadowScope
 import ir.kaaveh.sdpcompose.sdp
+import ir.kaaveh.sdpcompose.ssp
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductDetailScreen(productId: Int, viewModel: ProductDetailViewModel = koinViewModel()) {
+    val scope = rememberCoroutineScope()
     val product = viewModel.product.collectAsState()
     val uiState = viewModel.uiState.collectAsState()
     val navController = LocalParentNavController.current
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val tabs = listOf(R.string.info, R.string.warranty, R.string.reviews, R.string.dimension)
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { tabs.size })
 
     LaunchedEffect(productId) {
         if (viewModel.product.value == null) viewModel.getProductDetail(productId.toString())
@@ -58,7 +86,8 @@ fun ProductDetailScreen(productId: Int, viewModel: ProductDetailViewModel = koin
             ProductDetailTopBar(
                 scrollBehavior = scrollBehavior,
                 product = product.value!!,
-                onBackPressed = { navController?.popBackStack() }
+                onBackPressed = { navController?.popBackStack() },
+                onFavToggle = { product.value?.isFavourite = it }
             )
         }
     ) { innerPadding ->
@@ -83,18 +112,137 @@ fun ProductDetailScreen(productId: Int, viewModel: ProductDetailViewModel = koin
                         shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
                     )
             ) {
-                items(100) { index ->
-                    if (index == 0) Spacer(modifier = Modifier.height(10.sdp))
-                    Text(
-                        "$index",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 10.sdp)
-                            .height(20.sdp)
-                    )
-                    Spacer(modifier = Modifier.height(10.sdp))
+                item {
+                    Column(modifier = Modifier.padding(horizontal = 8.sdp, vertical = 10.sdp)) {
+                        Text(
+                            text = product.value?.title ?: "",
+                            fontSize = 14.ssp,
+                            lineHeight = 14.ssp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontWeight = FontWeight.SemiBold,
+                        )
+
+                        Spacer(modifier = Modifier.height(10.sdp))
+
+                        Text(
+                            text = product.value?.description ?: "",
+                            fontSize = 11.ssp,
+                            lineHeight = 11.ssp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontWeight = FontWeight.Medium,
+                        )
+                    }
+                }
+
+                stickyHeader {
+                    TabRow(containerColor = Color.Transparent, selectedTabIndex = pagerState.currentPage) {
+                        tabs.forEachIndexed { index, title ->
+                            Tab(
+                                selected = pagerState.currentPage == index,
+                                onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
+                                unselectedContentColor = MaterialTheme.colorScheme.onBackground,
+                                text = {
+                                    Text(
+                                        fontSize = 11.ssp,
+                                        lineHeight = 11.ssp,
+                                        text = stringResource(title),
+                                        fontWeight = FontWeight.Medium,
+                                    )
+                                }
+                            )
+                        }
+                    }
+                }
+
+                item {
+                    HorizontalPager(state = pagerState, modifier = Modifier.fillParentMaxHeight()) { page ->
+                        when (page) {
+                            0 -> Box { }
+                            1 -> Box { }
+                            2 -> Box { }
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@SuppressLint("ViewModelConstructorInComposable")
+@Preview(showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun ProductDetailPreview() {
+    val context = LocalContext.current
+
+    ProductDetailScreen(
+        -1, ProductDetailViewModel(
+            GetProductDetailUseCase(
+                ProductDetailRemoteRepoImpl(
+                    context,
+                    createApiService(createRetrofit(createOkHttpClient()))
+                )
+            )
+        ).apply {
+            product.value = Product(
+                id = 1,
+                title = "Essence Mascara Lash Princess",
+                description = "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+                category = "beauty",
+                price = 9.99,
+                discountPercentage = 10.48,
+                rating = 2.56,
+                stock = 99,
+                tags = listOf("beauty", "mascara"),
+                brand = "Essence",
+                sku = "BEA-ESS-ESS-001",
+                weight = 4,
+                dimensions = Dimensions(
+                    width = 15.14,
+                    height = 13.08,
+                    depth = 22.99
+                ),
+                warrantyInformation = "1 week warranty",
+                shippingInformation = "Ships in 3-5 business days",
+                availabilityStatus = "In Stock",
+                reviews = listOf(
+                    Review(
+                        rating = 3,
+                        comment = "Would not recommend!",
+                        date = "2025-04-30T09:41:02.053Z",
+                        reviewerName = "Eleanor Collins",
+                        reviewerEmail = "eleanor.collins@x.dummyjson.com"
+                    ),
+                    Review(
+                        rating = 4,
+                        comment = "Very satisfied!",
+                        date = "2025-04-30T09:41:02.053Z",
+                        reviewerName = "Lucas Gordon",
+                        reviewerEmail = "lucas.gordon@x.dummyjson.com"
+                    ),
+                    Review(
+                        rating = 5,
+                        comment = "Highly impressed!",
+                        date = "2025-04-30T09:41:02.053Z",
+                        reviewerName = "Eleanor Collins",
+                        reviewerEmail = "eleanor.collins@x.dummyjson.com"
+                    )
+                ),
+                returnPolicy = "No return policy",
+                minimumOrderQuantity = 48,
+                meta = Meta(
+                    createdAt = "2025-04-30T09:41:02.053Z",
+                    updatedAt = "2025-04-30T09:41:02.053Z",
+                    barcode = "5784719087687",
+                    qrCode = "https://cdn.dummyjson.com/public/qr-code.png"
+                ),
+                images = listOf(
+                    "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp",
+                    "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp",
+                    "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp",
+                    "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
+                ),
+                thumbnail = "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
+            )
+        }
+    )
 }
--- app/src/main/java/com/example/myapplication/ui/product_detail/presentation/components/ProductDetailTopBar.kt
+++ app/src/main/java/com/example/myapplication/ui/product_detail/presentation/components/ProductDetailTopBar.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication.ui.product_detail.presentation.components
 
-import android.annotation.SuppressLint
-import android.content.res.Configuration
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,36 +13,40 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.zIndex
-import com.example.myapplication.models.response.product.Dimensions
-import com.example.myapplication.models.response.product.Meta
 import com.example.myapplication.models.response.product.Product
-import com.example.myapplication.models.response.product.Review
 import com.example.myapplication.utils.components.NetworkImage
 import com.example.myapplication.utils.components.SvgImage
 import ir.kaaveh.sdpcompose.sdp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProductDetailTopBar(scrollBehavior: TopAppBarScrollBehavior, product: Product, onBackPressed: () -> Unit) {
+fun ProductDetailTopBar(
+    scrollBehavior: TopAppBarScrollBehavior,
+    product: Product,
+    onBackPressed: () -> Unit,
+    onFavToggle: (Boolean) -> Unit
+) {
     val collapsedAppBarHeightPx = remember { mutableIntStateOf(0) }
     val expandedAppBarHeightPx = remember { mutableIntStateOf(0) }
     var maxHeightPx = collapsedAppBarHeightPx.intValue + expandedAppBarHeightPx.intValue
@@ -67,7 +72,7 @@ fun ProductDetailTopBar(scrollBehavior: TopAppBarScrollBehavior, product: Produc
         content = {
             Box(
                 modifier = Modifier.fillMaxWidth(),
-                content = { CollapsedAppBar(product, onBackPressed) }
+                content = { CollapsedAppBar(product, onBackPressed, onFavToggle) }
             )
 
             Box(
@@ -98,7 +103,7 @@ fun ProductDetailTopBar(scrollBehavior: TopAppBarScrollBehavior, product: Produc
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun CollapsedAppBar(product: Product, onBackPressed: () -> Unit) {
+private fun CollapsedAppBar(product: Product, onBackPressed: () -> Unit, onFavToggle: (Boolean) -> Unit) {
     val isFavourite = remember { mutableStateOf(product.isFavourite ?: false) }
 
     Row(modifier = Modifier.padding(top = 40.sdp, bottom = 12.sdp, start = 10.sdp, end = 10.sdp)) {
@@ -125,7 +130,10 @@ private fun CollapsedAppBar(product: Product, onBackPressed: () -> Unit) {
                 .clip(CircleShape)
                 .background(MaterialTheme.colorScheme.onSurface)
                 .size(35.sdp)
-                .clickable { onBackPressed() },
+                .clickable {
+                    isFavourite.value = !isFavourite.value
+                    onFavToggle.invoke(isFavourite.value)
+                },
         ) {
             SvgImage(
                 asset = if (isFavourite.value) "fav_filled" else "fav_outline",
@@ -139,86 +147,49 @@ private fun CollapsedAppBar(product: Product, onBackPressed: () -> Unit) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ExpandedAppBar(product: Product) {
-    val pagerState = rememberPagerState { product.images?.size ?: 0 }
-    HorizontalPager(
-        state = pagerState,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 10.sdp)
-            .height(200.sdp)
-    ) { page ->
-        NetworkImage(
-            showShimmerWhenLoading = false,
-            imageUrl = product.images?.get(page) ?: "",
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
-}
+    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { product.images?.size ?: 0 })
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("ViewModelConstructorInComposable")
-@Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES, heightDp = 200)
-@Composable
-private fun PreviewProductDetailTopBar() {
-    ProductDetailTopBar(
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(),
-        Product(
-            id = 1,
-            title = "Essence Mascara Lash Princess",
-            description = "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-            category = "beauty",
-            price = 9.99,
-            discountPercentage = 10.48,
-            rating = 2.56,
-            stock = 99,
-            tags = listOf("beauty", "mascara"),
-            brand = "Essence",
-            sku = "BEA-ESS-ESS-001",
-            weight = 4,
-            dimensions = Dimensions(
-                width = 15.14,
-                height = 13.08,
-                depth = 22.99
-            ),
-            warrantyInformation = "1 week warranty",
-            shippingInformation = "Ships in 3-5 business days",
-            availabilityStatus = "In Stock",
-            reviews = listOf(
-                Review(
-                    rating = 3,
-                    comment = "Would not recommend!",
-                    date = "2025-04-30T09:41:02.053Z",
-                    reviewerName = "Eleanor Collins",
-                    reviewerEmail = "eleanor.collins@x.dummyjson.com"
-                ),
-                Review(
-                    rating = 4,
-                    comment = "Very satisfied!",
-                    date = "2025-04-30T09:41:02.053Z",
-                    reviewerName = "Lucas Gordon",
-                    reviewerEmail = "lucas.gordon@x.dummyjson.com"
-                ),
-                Review(
-                    rating = 5,
-                    comment = "Highly impressed!",
-                    date = "2025-04-30T09:41:02.053Z",
-                    reviewerName = "Eleanor Collins",
-                    reviewerEmail = "eleanor.collins@x.dummyjson.com"
+    Box {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.sdp)
+                .height(200.sdp)
+        ) { page ->
+            NetworkImage(
+                showShimmerWhenLoading = false,
+                imageUrl = product.images?.get(page) ?: "",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .wrapContentWidth()
+                .align(alignment = Alignment.BottomCenter)
+                .padding(bottom = 10.sdp),
+        ) {
+            product.images?.forEachIndexed { index, _ ->
+                val isSelected = pagerState.currentPage == index
+                val size = animateDpAsState(targetValue = if (isSelected) 8.sdp else 8.sdp)
+                val color = animateColorAsState(
+                    targetValue = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.primary
                 )
-            ),
-            returnPolicy = "No return policy",
-            minimumOrderQuantity = 48,
-            meta = Meta(
-                createdAt = "2025-04-30T09:41:02.053Z",
-                updatedAt = "2025-04-30T09:41:02.053Z",
-                barcode = "5784719087687",
-                qrCode = "https://cdn.dummyjson.com/public/qr-code.png"
-            ),
-            images = listOf(
-                "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-            ),
-            thumbnail = "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/thumbnail.webp"
-        )
-    ) { }
+
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(size.value)
+                        .background(color.value)
+                        .clickable { scope.launch { pagerState.animateScrollToPage(index) } }
+                )
+
+                if (index != product.images.lastIndex) Spacer(modifier = Modifier.width(2.sdp))
+            }
+        }
+    }
 }
